d16b5341c48b978149af7cbe7bfdad2d6a1ab5eb
unknown <fkca.av15.gev@gmail.com>
2021-11-11 16:18:52 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 let burger = document.querySelector(".burger");
 let container = document.getElementById('container')
+let sidebar = document.getElementById('sidebar')
 burger.addEventListener("click", () => {
     burger.classList.toggle("activebrgr");
     container.classList.toggle("activeContainer");
+    sidebar.classList.toggle('activeSidebar');
 })
